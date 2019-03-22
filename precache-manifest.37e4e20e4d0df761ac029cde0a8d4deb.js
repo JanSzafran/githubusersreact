6c878ece0d5e11a419445080a3dcd5cfcdd9e5d6
JanSzaf <j.szafran.praca@gmail.com>
2019-03-22 11:49:46 +0100

    Updates

---
+++ precache-manifest.37e4e20e4d0df761ac029cde0a8d4deb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "048d518254741f90822a",
+    "url": "/githubusersreact/static/js/runtime~main.3444ccfe.js"
+  },
+  {
+    "revision": "e87023abd4a0d503f52c",
+    "url": "/githubusersreact/static/js/main.7f84c487.chunk.js"
+  },
+  {
+    "revision": "a1de36d8a4b30e470834",
+    "url": "/githubusersreact/static/js/2.b23a61f0.chunk.js"
+  },
+  {
+    "revision": "e87023abd4a0d503f52c",
+    "url": "/githubusersreact/static/css/main.079838d7.chunk.css"
+  },
+  {
+    "revision": "a1de36d8a4b30e470834",
+    "url": "/githubusersreact/static/css/2.9a46441d.chunk.css"
+  },
+  {
+    "revision": "9cd0888e3f68773fe8378178eb90181a",
+    "url": "/githubusersreact/index.html"
+  }
+];
